3042640c70fa5c9dbf3606834b0d3494571d2923
luferjuhe <lufer_juhe@hotmail.com>
2024-11-25 23:17:54 -0600

    Initial Commit

---
+++ Agency.Data/DbContextAgency.cs
@@ -0,0 +1,21 @@
+﻿using Agency.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agency.Data;
+public class DbContextAgency : DbContext
+{
+    private readonly string connectionString;
+
+    public DbContextAgency(DbContextOptions<DbContextAgency> options) : base(options)
+    {
+
+    }
+
+    public DbContextAgency(string connectionString)
+    {
+        this.connectionString = connectionString;
+    }
+
+    public DbSet<User> Users { get; set; }
+}
+
